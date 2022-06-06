3e189c6c3c2b3e2059d8141bb93e3ba3bba9a6a7
Gadaba21 <106908462+Gadaba21@users.noreply.github.com>
2022-06-06 21:20:24 +0300

    module2-task1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const NUMBOR = 6
+function vozvratchisla(from, before)
+{ 
+    let proverka
+     if (from>NUMBOR || before<NUMBOR )
+     {proverka = 'Нет такого числа в диопозоне'}
+     else {proverka = 'Число есть в диопозоне, '+ NUMBOR}
+return proverka
+}
+vozvratchisla(2,5)
+
+
+
+
+function examination(lineNumber, maximumLength)
+{
+    let comparison=lineNumber<maximumLength
+    return comparison
+}
+
+examination(21,20)
+
+
