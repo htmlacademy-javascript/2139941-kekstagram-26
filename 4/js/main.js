d82f0a4767b5b1ae34c862b75f9039950491ceb3
Keks <keks@htmlacademy.ru>
2022-06-07 20:10:20 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,23 +1,17 @@
-const NUMBOR = 6
+const NUMBER = 5;
 function vozvratchisla(from, before)
-{ 
-    let proverka
-     if (from>NUMBOR || before<NUMBOR )
-     {proverka = 'Нет такого числа в диопозоне'}
-     else {proverka = 'Число есть в диопозоне, '+ NUMBOR}
-return proverka
-}
-vozvratchisla(2,5)
-
-
-
+{
+  let proverka
+  if (from>NUMBER || before<NUMBER )
+  {proverka = 'Нет такого числа в диапазоне'}
+  else {proverka = `Число есть в диапазоне, ${NUMBER}`}
+  return proverka
+};
+vozvratchisla(2,5);
 
 function examination(lineNumber, maximumLength)
 {
-    let comparison=lineNumber<maximumLength
-    return comparison
-}
-
-examination(21,20)
-
-
+  let comparison=lineNumber<=maximumLength
+  return comparison
+};
+examination(21,20);
