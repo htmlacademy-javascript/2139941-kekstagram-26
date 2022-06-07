bcf8312aa2d658586ac9256b18fe4d54f91b350d
Gadaba21 <106908462+Gadaba21@users.noreply.github.com>
2022-06-07 20:08:06 +0300

    Дедактирование текста

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const NUMBER = 5;
 function vozvratchisla(from, before)
 {
-let proverka
+  let proverka
   if (from>NUMBER || before<NUMBER )
   {proverka = 'Нет такого числа в диапазоне'}
   else {proverka = `Число есть в диапазоне, ${NUMBER}`}
@@ -9,14 +9,9 @@ let proverka
 };
 vozvratchisla(2,5);
 
-
-
-
 function examination(lineNumber, maximumLength)
 {
-let comparison=lineNumber<=maximumLength
+  let comparison=lineNumber<=maximumLength
   return comparison
 };
 examination(21,20);
-
-
