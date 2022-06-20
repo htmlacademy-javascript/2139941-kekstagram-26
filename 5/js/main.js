e5b5797d753b3de43fdea153466e99e5623bfd94
Keks <keks@htmlacademy.ru>
2022-06-20 04:53:09 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,86 +1,82 @@
-const NAME = ['Вася','Кекс', 'Ульдир', 'Гулдан', 'Аня', 'Повета', 'Ирган', 'Пехтор'];
-const MESSAGE = ['Всё отлично!',
+const USER_NAMES = [
+  'Вася',
+  'Кекс',
+  'Ульдир',
+  'Гулдан',
+  'Аня',
+  'Повета',
+  'Ирган',
+  'Пехтор',
+];
+
+const POST_MESSAGES = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const DESCRIPTION= ['Я отдыхаю','Веселье!','Жара','Улетное спасение','Котофото','Какой я красавец','Круче просто нету','Что-то непонятное',
-  'Увидемся завтра','Непоймаешь','Вот это хвост','А я сегодня мышку сьел','Вернулся от киски','Нечего себе','Мурки','Эпичный прыжок','Только встал','А тебе слабо?',
-  'Потягушки','Наверное это про меня','Самый улетный кот на свете','Сейчас бы поесть','Спал весь день','Ночная охота','Неуловимый'];
-const userProfile = [];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
-function earchRandomNumber(from, before)
-{let NUMBER = Math.floor(Math.random()*(before+1));
-  if(from>before){
-    const g = from;
-    from = before;
-    before = g;
-  }
-  let randomNumber;
-  while (from>NUMBER)
-  {NUMBER = Math.floor(Math.random()*(before+1));}
-  randomNumber = NUMBER;
-  return randomNumber;}
+const POST_DESCRIPTIONS = [
+  'Я отдыхаю',
+  'Веселье!',
+  'Жара',
+  'Улетное спасение',
+  'Котофото',
+  'Какой я красавец',
+  'Круче просто нету',
+  'Что-то непонятное',
+  'Увидемся завтра',
+  'Непоймаешь',
+  'Вот это хвост',
+  'А я сегодня мышку сьел',
+  'Вернулся от киски',
+  'Нечего себе',
+  'Мурки',
+  'Эпичный прыжок',
+  'Только встал','А тебе слабо?',
+  'Потягушки',
+  'Наверное это про меня',
+  'Самый улетный кот на свете',
+  'Сейчас бы поесть',
+  'Спал весь день',
+  'Ночная охота',
+  'Неуловимый',
+];
 
-const  creatingNewId = function(){
-  for(let i=0;i<=24;i++){
-    userProfile.push({id : i+1});}
-  return userProfile;
-};
-const creatingNewUrl = function(){
-  for(let i=0;i<=24;i++){
-    userProfile[i].url = (`photos/${i+1}.jpg`);}
-  return userProfile;
-};
-const creatingNewDescription = function(){
-  for(let i=0;i<=24;i++){
-    userProfile[i].DESCRIPTION = (DESCRIPTION[i]);}
-  return userProfile;
-};
-const creatingNewLikes= function(){
-  for(let i=0;i<=24;i++){
-    userProfile[i].likes = (earchRandomNumber(15,200));}
-  return userProfile;
-};
 const comments = [];
-const creatingCommentsId = function(){
-  for(let i=0;i<=24;i++){
-    comments.push({id : (3*i+1)*earchRandomNumber(23,25)});}
-  return comments;
+
+const photoInformation = [];
+
+function generateRandomInteger (from, to) {
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+  return Math.floor(Math.random() * ((to + 1) - from) + from);
+}
+
+const sortingRandomNumber = function() {
+  return Math.random() - 0.5;
 };
 
-const creatingCommentsAvatar = function(){
-  for(let i=0;i<=24;i++){
-    comments[i].avatar =(`img/avatar-${earchRandomNumber(1,6)}.svg`);}
-  return comments;};
-const creatingCommentsMessage = function(){
-  for(let i=0;i<=24;i++){
-    comments[i].MESSAGE =(MESSAGE[(earchRandomNumber(1,6)-1)]);}
-  return comments;
+const creationPhotoInformation = function() {
+  const BLACK_BOX = [];
+  for (let i = 0; i <= 24; i++) {
+    photoInformation.push({id : i + 1});
+    photoInformation[i].url = (`photos/${i + 1}.jpg`);
+    photoInformation[i].description = (POST_DESCRIPTIONS[i]);
+    photoInformation[i].likes = (generateRandomInteger(15,200));
+    BLACK_BOX.push({id : i + 1});
+    BLACK_BOX.sort(sortingRandomNumber);
+    comments.push(BLACK_BOX[i]);
+    comments[i].avatar = (`img/avatar-${generateRandomInteger(1,6)}.svg`);
+    comments[i].message = (POST_MESSAGES[(generateRandomInteger(1,6) - 1)]);
+    comments[i].name = (USER_NAMES[(generateRandomInteger(1,8) - 1)]);
+    photoInformation[i].comments = comments[i];}
+  return photoInformation;
 };
-const creatingCommentsName = function(){
-  for(let i=0;i<=24;i++){
-    comments[i].NAME =(NAME[(earchRandomNumber(1,8)-1)]);}
-  return comments;
-};
-const creatingNewComments = function(){
-  for(let i=0;i<=24;i++){
-    userProfile[i].comments =(comments[i]);}
-  return userProfile;};
-const creationUserProfile = function(){
-  creatingNewId();
-  creatingNewUrl();
-  creatingNewDescription();
-  creatingNewLikes();
-  creatingCommentsId();
-  creatingCommentsAvatar();
-  creatingCommentsMessage();
-  creatingCommentsName();
-  creatingNewComments();
-}; //основной массив с профилями
-
-creationUserProfile();
+creationPhotoInformation();
 
 /*
 function examination(lineNumber, maximumLength)
