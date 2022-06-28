4fc74ed9d0da78fe22e2e73b829292e1419eb258
Gadaba21 <106908462+Gadaba21@users.noreply.github.com>
2022-06-28 22:43:29 +0300

    не доделал, пока не работает, возможно что то не так, осталось связать маленькие фото с большими

---
+++ js/big_picture.js
@@ -0,0 +1,43 @@
+import {setImgSrc,setElementText} from './picture.js';
+
+const searchElement = (element) => {
+  document.querySelector(element)
+};
+const displayElementremove = (element, display) => {
+  searchElement(element).classList.remove(display)
+};
+const displayElementadd = (element, display) => {
+  searchElement(element).classList.add(display)
+};
+const makeBigPhotoFromItem = (item) => {
+  const {
+  id,
+  url,
+  description,
+  likes,
+  comments
+  } = item;
+  setImgSrc(document.querySelector('.big-picture__img'),url);
+  setElementText(document.querySelector('.likes-count'),likes);
+  setElementText(document.querySelector('.comments-count'),comments.length);
+  setElementText(document.querySelector('.social__caption'),description);
+};
+console.log('.big-picture__img')
+export const displayBigPhoto = (item) => {
+  document.querySelector('.pictures.container').addEventListener('click',function (evt) {
+  evt.preventDefault();
+  displayElementremove('.big-picture','hidden')
+  makeBigPhotoFromItem(item)
+  displayElementremove('.social__comment-count','hidden')
+  displayElementremove('.comments-loader','hidden')
+  displayElementadd('.body', '.modal-open')
+})};
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    document.querySelector('.big-picture').classList.add('hidden');
+    displayElementremove('.body', '.modal-open')}});
+document.querySelector('.big-picture__cancel').addEventListener('click', function () {
+  displayElementadd('.big-picture','hidden');
+  displayElementremove('.body', '.modal-open');
+});
