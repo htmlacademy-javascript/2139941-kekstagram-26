59dedbc1f452352c807ff1f1d04193d596482d92
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-07-02 14:12:27 +0300

    inspection

--- js/big_picture.js
+++ js/big_picture.js
@@ -1,11 +1,11 @@
 import { setImgSrc, setElementText } from './picture.js';
-import {MAX_LENGHT_PHOTOS} from './data.js'
-import {isEscapeKey} from './util.js'
+import {MAX_LENGHT_PHOTOS} from './data.js';
+import {isEscapeKey} from './util.js';
 const displayElementremove = (element, display) => {
-  document.querySelector(element).classList.remove(display)
+  document.querySelector(element).classList.remove(display);
 };
 const displayElementadd = (element, display) => {
-  document.querySelector(element).classList.add(display)
+  document.querySelector(element).classList.add(display);
 };
 const makeBigPhotoFromItem = (item) => {
   const {
@@ -30,44 +30,44 @@ const commentBigPhoto = (template, item) => {
   } = item;
   setImgSrc(template.querySelector('.social__picture'), avatar, name);
   setElementText(template.querySelector('.social__text'), message);
-  return template
+  return template;
 };
 const createCollectcomment = (template) => (fragment, item) => {
-  fragment.append(commentBigPhoto(template.content.cloneNode(true), item))
-  return fragment
+  fragment.append(commentBigPhoto(template.content.cloneNode(true), item));
+  return fragment;
 };
 
 const displayUsercomment = (item, template) => item.reduce(createCollectcomment(template), document.createDocumentFragment());
 
 export const displayBigPhoto = (item) => {
-  const photoButton = document.querySelectorAll('.picture')
+  const photoButton = document.querySelectorAll('.picture');
   for (let j = 0; j < MAX_LENGHT_PHOTOS; j++) {
-    photoButton[j].addEventListener('click', function (evt) {
+    photoButton[j].addEventListener('click', (evt) => {
       evt.preventDefault();
       displayElementremove('.big-picture', 'hidden');
       makeBigPhotoFromItem(item[j]);
       document.querySelector('.social__comments')
-      .append(displayUsercomment(item[j].comments, document.querySelector('#social')))
+        .append(displayUsercomment(item[j].comments, document.querySelector('#social')));
       displayElementadd('body', 'modal-open');
-    })
+    });
   }
 };
 const closeBigPhoto = () => {
-  document.addEventListener('keydown', function (evt) {
+  document.addEventListener('keydown', (evt) => {
     if (isEscapeKey(evt)) {
       displayElementadd('.big-picture', 'hidden');
-      displayElementremove('body', 'modal-open')
-      document.querySelectorAll('.social__comment').forEach(e=>e.remove())
-    }})}
+      displayElementremove('body', 'modal-open');
+      document.querySelectorAll('.social__comment').forEach((e)=>e.remove());
+    }});};
 const closeBigPhoto2 = () => {
-  document.querySelector('.big-picture__cancel').addEventListener('click', function () {
+  document.querySelector('.big-picture__cancel').addEventListener('click', () => {
     displayElementadd('.big-picture', 'hidden');
     displayElementremove('body', 'modal-open');
-    document.querySelectorAll('.social__comment').forEach(e=>e.remove())
- })}
+    document.querySelectorAll('.social__comment').forEach((e)=>e.remove());
+  });};
 export const removeBigPhoto = () => {
-  closeBigPhoto()
-  document.removeEventListener('keydown', closeBigPhoto())
-  closeBigPhoto2()
-  document.querySelector('.big-picture__cancel').removeEventListener('click', closeBigPhoto2())
-}
+  closeBigPhoto();
+  document.removeEventListener('keydown', closeBigPhoto());
+  closeBigPhoto2();
+  document.querySelector('.big-picture__cancel').removeEventListener('click', closeBigPhoto2());
+};
--- js/util.js
+++ js/util.js
@@ -12,12 +12,10 @@ export function generateRandomInteger (from, to) {
     throw new Error(`from must be in order ${from},${to}`);
   }
   return Math.floor(Math.random() * (to - from) + from);
-};
+}
 
 export const gerRandomItem = (items) => items[generateRandomInteger(0, items.length)];
 
 export const generateArray = (length, generator) => Array.from({length},(_,ix) => generator(ix));
 
-export const isEscapeKey = (evt) => {
-  return evt.key === 'Escape';
-};
+export const isEscapeKey = (evt) => evt.key === 'Escape';
